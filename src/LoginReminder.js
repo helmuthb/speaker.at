710fbce1e13aec01d6291e5b86966fd3610d9744
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2018-09-15 00:51:38 +0200

    Adjusted color of login-warning

--- src/LoginReminder.js
+++ src/LoginReminder.js
@@ -3,9 +3,12 @@ import { withStyles } from '@material-ui/core/styles';
 import { connect } from '@cerebral/react';
 import { Paper } from '@material-ui/core';
 import { state, signal } from 'cerebral/tags';
+import yellow from '@material-ui/core/colors/yellow';
+import ErrorIcon from '@material-ui/icons/Error';
 
 const styles = theme => ({
   paper: {
+    backgroundColor: yellow[100],
     marginTop: theme.spacing.unit * 3,
     // marginBottom: theme.spacing.unit * 3,
     padding: theme.spacing.unit * 2,
@@ -14,6 +17,11 @@ const styles = theme => ({
       // marginBottom: theme.spacing.unit * 6,
       padding: theme.spacing.unit * 3
     }
+  },
+  icon: {
+    marginBottom: -5,
+    marginRight: theme.spacing.unit * 2,
+    display: 'inline'
   }
 });
 
@@ -21,11 +29,16 @@ class LoginReminder extends React.Component {
   render() {
     const { classes, auth } = this.props;
 
-    return (
-      <Paper className={classes.paper}>
-        Please login to submit talk proposals.
-      </Paper>
-    );
+    if (!auth.loggedIn) {
+      return (
+        <Paper className={classes.paper}>
+          <ErrorIcon color="error" className={classes.icon} />
+          Please login to submit talk proposals.
+        </Paper>
+      );
+    } else {
+      return '';
+    }
   }
 }
 
@@ -37,4 +50,4 @@ const ConnectedLoginReminder = connect(
   LoginReminder
 );
 
-export default withStyles(styles)(LoginReminder);
+export default withStyles(styles)(ConnectedLoginReminder);
